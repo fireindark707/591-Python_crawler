3e0c52475c3d3cb57dde63ac6599dbad2dd2c321
landen3080 <zhangyu@1234-2.local>
2018-04-02 22:45:34 +0800

    刪掉一些沒用的test，增加chrome_selenium爬蟲
    成功抓取台北租屋第一頁的所有房源URL，還需要去除頭尾的\n和大量空格

---
+++ webdriver_chrome.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  2 21:08:05 2018
+
+@author: zhangyu
+"""
+
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import bs4
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://rent.591.com.tw/?kind=0&region=1")
+html = driver.page_source
+
+page = BeautifulSoup(html,"lxml")
+nameList = page.findAll("h3")
+
+url_list = []
+for name in nameList:
+    url = name.a.attrs["href"]
+    url_list.append(url)
+    print(url)
+
+    
