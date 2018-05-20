9248d190bc9071c6bd9aaaa7eed7ee9446e05174
Proletarian Library <zhangyu@1234-2.local>
2018-05-20 15:16:47 +0800

    更新換頁方法

--- webdriver_chrome.py
+++ webdriver_chrome.py
@@ -13,15 +13,22 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
 
+driver.get("https://rent.591.com.tw")
 driver.get("https://rent.591.com.tw/?kind=0&region=1")
-html = driver.page_source
 
-page = BeautifulSoup(html, "lxml")
-nameList = page.findAll("h3")
 
-url_list = []
-for name in nameList:
-    url = name.a.attrs["href"]
-    url = url.strip()
-    url_list.append(url)
-    print(url)
+for num in range(1,20):
+    html = driver.page_source
+    element = driver.find_element_by_class_name("pageNext")
+    driver.execute_script("arguments[0].click();", element)
+
+    page = BeautifulSoup(html, "lxml")
+    nameList = page.findAll("h3")
+
+    for name in nameList:
+        url = name.a.attrs["href"]
+        url = url.strip()
+        url_list.append(url)
+        print(url)
+        
+        
