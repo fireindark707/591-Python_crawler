b6c73a2bea822249fc64b9939c1916bc97f5fa38
Proletarian Library <zhangyu@1234-2.local>
2018-05-20 17:43:13 +0800

    update df

--- set_up_table.py
+++ set_up_table.py
@@ -13,10 +13,44 @@ url_list = pickle.load(open("./data/url_list_5_20.dat", "rb"))
 
 length = len(url_list)
 
-rentDf = pd.DataFrame(np.random.randn(length,30),columns = ['編號','價格','坪數','樓層','總樓層數','型態','種類','地址','押金','車位','管理費','最短租期','養寵物','身分要求','性別要求','朝向','格局','桌子','衣櫃','床','熱水器','天然瓦斯','電視','冰箱','冷氣','洗衣機','椅子','沙發','網絡','第四台'])
+rent_dict = {
+        '編號':[0]*length,
+        '價格':[0]*length,
+        '坪數':[0]*length,
+        '樓層':[0]*length,
+        '總樓層數':[0]*length,
+        '型態':[0]*length,
+        '種類':[0]*length,
+        '地址':[0]*length,
+        '押金':[0]*length,
+        '車位':[0]*length,
+        '管理費':[0]*length,
+        '最短租期':[0]*length,
+        '養寵物':[0]*length,
+        '身分要求':[0]*length,
+        '性別要求':[0]*length,
+        '朝向':[0]*length,
+        '格局':[0]*length,
+        '桌子':[0]*length,
+        '衣櫃':[0]*length,
+        '床':[0]*length,
+        '熱水器':[0]*length,
+        '天然瓦斯':[0]*length,
+        '電視':[0]*length,
+        '冰箱':[0]*length,
+        '冷氣':[0]*length,
+        '洗衣機':[0]*length,
+        '椅子':[0]*length,
+        '沙發':[0]*length,
+        '網絡':[0]*length,
+        '第四台':[0]*length
+}
+
+rentDf = pd.DataFrame(rent_dict)
 
 #抓取編號
 for i in range(0,length):
     bianHao = url_list[i][30:37]
     rentDf.xs(i)['編號'] = bianHao
     
+pickle.dump(url_list, open("./data/rent_table.dat", "wb"))
